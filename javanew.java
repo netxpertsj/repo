efc5a4bc40eedd407535c9d92e1fba583e86a62e
Ubuntu <ubuntu@ip-172-31-36-37.ap-south-1.compute.internal>
2024-07-19 00:45:44 +0000

    5th Line Added

--- javanew.java
+++ javanew.java
@@ -5,3 +5,8 @@ boolean myBool = true;       // Boolean
 String myText = "Hello";     // String
 boolean isJavaFun = true;
 System.out.println("20 is greater than 18");
+int x = 20;
+int y = 18;
+if (x > y) {
+  System.out.println("x is greater than y");
+}
