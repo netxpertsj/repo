55b8dfa1fd91b722a4679afdc9604f695c28234d
Ubuntu <ubuntu@ip-172-31-36-37.ap-south-1.compute.internal>
2024-07-19 04:13:04 +0000

    New Java Module

--- javanew.java
+++ javanew.java
@@ -10,3 +10,8 @@ int y = 18;
 if (x > y) {
   System.out.println("x is greater than y");
 }
+int myNum = 5;               // Integer (whole number)
+float myFloatNum = 5.99f;    // Floating point number
+char myLetter = 'D';         // Character
+boolean myBool = true;       // Boolean
+String myText = "Hello";     // String
