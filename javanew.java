943eb0f02e2be385d025cd0d00ed8d2996d4aebf
John <john@gmail.com>
2024-07-26 00:19:15 +0000

    One Added

--- javanew.java
+++ javanew.java
@@ -15,3 +15,4 @@ float myFloatNum = 5.99f;    // Floating point number
 char myLetter = 'D';         // Character
 boolean myBool = true;       // Boolean
 String myText = "Hello";     // String
+One
